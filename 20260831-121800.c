4196a18b681552c8d374367a9bc1e30ae7c813bf
guptakaushal03 <babukaushal03@gmail.com>
2026-08-31 12:20:55 +0530

    Add files via upload

---
+++ 20260831-121800.c
@@ -0,0 +1,14 @@
+//Q1 (User Inputs, Operations & Output
+//Write a program to input two numbers and display their sum.
+
+#include <stdio.h>
+int main() {
+    int a,b;
+    printf("ENTER NUMBER 1: ");
+    scanf("%d",&a);
+    printf("ENTER NUMBER 2: ");
+    scanf("%d",&b);
+    printf("THE SUM OF %d and %d is %d ", a,b,a+b);
+    return 0;
+    }
+    
