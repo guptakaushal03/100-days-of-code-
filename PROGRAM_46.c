d0f1bba428efe8dd10468460f32dfafcf490003c
guptakaushal03 <babukaushal03@gmail.com>
2026-09-07 01:02:30 +0530

    Add files via upload

---
+++ PROGRAM_46.c
@@ -0,0 +1,14 @@
+// Write a program to print the following pattern:
+// *****
+// *****
+// *****
+// *****
+// *****
+
+#include <stdio.h>
+int main() {
+    for(int i=1;i<=5;i++){
+       printf("*****\n");
+    }
+return 0;
+}
